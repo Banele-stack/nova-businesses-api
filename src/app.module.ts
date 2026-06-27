bf8b1233bcdf2137f08b226824815042dc7e839f
Banele Ngubane <84304786+Banele-stack@users.noreply.github.com>
2026-06-27 13:26:25 +0200

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { BusinessModule } from "./business/business.module";
+import { Business } from "./business/entities/business.entity";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Business],
+      synchronize: true,
+    }),
+
+    BusinessModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
