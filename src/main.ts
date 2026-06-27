96033cfdda647e5a086a11766cd01c833986b955
Banele Ngubane <84304786+Banele-stack@users.noreply.github.com>
2026-06-27 14:37:06 +0200

    Fixed CORS again

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
       "http://localhost:3000",
       "http://localhost:3001",
       "http://localhost:3002",
+      "https://cosmobusiness.banelengubane.dev",
     ],
     credentials: true,
   });
